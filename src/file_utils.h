673932c71c9360493d2f332df83c88ec5c79f865
Hellboy <136455069+Victor3637@users.noreply.github.com>
2024-10-29 14:33:56 +0200

    Update file_utils.h

--- src/file_utils.h
+++ src/file_utils.h
@@ -1,1 +1,11 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
 
+#include <string>
+#include <vector>
+#include "cache.h"
+
+// Функція для отримання інформації про файли з певним розширенням у директорії
+std::vector<FileInfo> getFilesInfo(const std::string& directory, const std::string& extension);
+
+#endif // FILE_UTILS_H
