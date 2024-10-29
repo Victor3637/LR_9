e1434a8570e1f597f8654ca772f1697d0399c8e2
Hellboy <136455069+Victor3637@users.noreply.github.com>
2024-10-29 14:35:18 +0200

    Update ipc_utils.h

--- src/ipc_utils.h
+++ src/ipc_utils.h
@@ -1,1 +1,14 @@
+#ifndef IPC_UTILS_H
+#define IPC_UTILS_H
 
+#include <string>
+#include <vector>
+#include "cache.h"
+
+// Функція для відправки запиту від клієнта на сервер
+bool sendRequestToServer(const std::string& directory, const std::string& extension, int port, std::string& response);
+
+// Функція для обробки запиту на сервері та повернення результату клієнту
+std::string handleClientRequest(int client_socket, Cache& cache);
+
+#endif // IPC_UTILS_H
